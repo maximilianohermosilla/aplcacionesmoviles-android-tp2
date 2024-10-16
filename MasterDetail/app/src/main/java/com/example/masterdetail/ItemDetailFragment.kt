4aa8777a095de164095636d1eda784ec56836883
maximiliano_hermosilla <maximiliano_hermosilla@hotmail.com>
2024-10-16 15:36:03 -0300

    App MasterDetail funcionando

---
+++ MasterDetail/app/src/main/java/com/example/masterdetail/ItemDetailFragment.kt
@@ -0,0 +1,81 @@
+package com.example.masterdetail
+
+import android.content.ClipData
+import android.os.Bundle
+import android.view.DragEvent
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import com.example.masterdetail.placeholder.PlaceholderContent
+import com.example.masterdetail.databinding.FragmentItemDetailBinding
+import com.google.android.material.appbar.CollapsingToolbarLayout
+
+/**
+ * A fragment representing a single Item detail screen.
+ * This fragment is either contained in a [ItemListFragment]
+ * in two-pane mode (on larger screen devices) or self-contained
+ * on handsets.
+ */
+class ItemDetailFragment : Fragment() {
+
+    /**
+     * The placeholder content this fragment is presenting.
+     */
+    private var item: PlaceholderContent.PlaceholderItem? = null
+
+    var webview: WebView? = null
+
+    private val dragListener = View.OnDragListener { v, event ->
+        if (event.action == DragEvent.ACTION_DROP) {
+            val clipDataItem: ClipData.Item = event.clipData.getItemAt(0)
+            val dragData = clipDataItem.text
+            item = PlaceholderContent.ITEM_MAP[dragData]
+            updateContent()
+        }
+        true
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        arguments?.let {
+            if (it.containsKey(ARG_ITEM_ID)) {
+                // Load the placeholder content specified by the fragment
+                // arguments. In a real-world scenario, use a Loader
+                // to load content from a content provider.
+                item = PlaceholderContent.ITEM_MAP[it.getString(ARG_ITEM_ID)]
+            }
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view: View = inflater.inflate(R.layout.fragment_item_detail, container, false)
+        webview = view.findViewById<View>(R.id.wb_web) as WebView
+
+        updateContent()
+        return view;
+    }
+
+    private fun updateContent() {
+        webview?.setWebViewClient(WebViewClient())
+
+        webview?.clearCache(true)
+        webview?.clearHistory()
+        webview?.loadUrl(item?.website_url.toString())
+    }
+
+    companion object {
+        /**
+         * The fragment argument representing the item ID that this fragment
+         * represents.
+         */
+        const val ARG_ITEM_ID = "item_id"
+    }
+
+}
